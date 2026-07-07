42b4ec1f6520b2c0ab8b9c3113f8e11485df86dc
robin-busaragroup <robin-busaragroup@users.noreply.github.com>
2026-07-07 03:58:43 +0100

    Register AO3Chat routes during plugin initialization

--- plugin.rb
+++ plugin.rb
@@ -21,9 +21,41 @@ after_initialize do
   require_relative "lib/ao3_fanfic_forum/fields"
   require_relative "lib/ao3_fanfic_forum/normalizer"
   require_relative "lib/ao3_fanfic_forum/supporter_access"
+  require_relative "app/controllers/ao3_fanfic_forum/base_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/account_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/crypto_payments_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/login_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/logout_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/metadata_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/password_reset_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/room_requests_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/signup_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/supporter_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/supporter_status_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/terms_controller"
+  require_relative "app/controllers/ao3_fanfic_forum/topics_controller"
   require_relative "app/models/ao3_fanfic_forum/topic_metadata"
   require_relative "app/models/ao3_fanfic_forum/topic_term"
   require_relative "lib/ao3_fanfic_forum/metadata"
+
+  Discourse::Application.routes.prepend do
+    get "/ao3-fanfic/account" => "ao3_fanfic_forum/account#show"
+    post "/ao3-fanfic/crypto-payments" => "ao3_fanfic_forum/crypto_payments#create"
+    get "/ao3-fanfic/advanced-login" => "static#show", id: "login"
+    get "/ao3-fanfic/login" => "ao3_fanfic_forum/login#show"
+    post "/ao3-fanfic/logout" => "ao3_fanfic_forum/logout#create"
+    get "/ao3-fanfic/password-reset" => "ao3_fanfic_forum/password_reset#show"
+    post "/ao3-fanfic/room-requests" => "ao3_fanfic_forum/room_requests#create"
+    get "/ao3-fanfic/signup" => "ao3_fanfic_forum/signup#show"
+    get "/ao3-fanfic/supporter" => "ao3_fanfic_forum/supporter#show"
+    get "/ao3-fanfic/supporter-status" => "ao3_fanfic_forum/supporter_status#show"
+    get "/ao3-fanfic/terms" => "ao3_fanfic_forum/terms#index"
+    get "/ao3-fanfic/topics" => "ao3_fanfic_forum/topics#index"
+    put "/ao3-fanfic/topics/:topic_id/metadata" => "ao3_fanfic_forum/metadata#update"
+    get "/login" => redirect("#{Discourse.base_path}/ao3-fanfic/login")
+    get "/password-reset" => redirect("#{Discourse.base_path}/ao3-fanfic/password-reset")
+    get "/signup" => redirect("#{Discourse.base_path}/ao3-fanfic/signup")
+  end
 
   Ao3FanficForum::AuthConfiguration.apply!
 
